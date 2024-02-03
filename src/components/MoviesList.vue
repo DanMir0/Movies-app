daf76ebb62bc79a85f894e4cc8335d0036f70f83
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2024-02-03 20:08:50 +0300

    style list is empty

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -13,13 +13,31 @@ const props = defineProps({
   <div v-if="movies.length > 0" class="lists">
       <movie-item :movie="movie" v-for="movie in movies" :key="movie.id"></movie-item>
   </div>
-  <div v-else>
-    <h3 style="color: red;">Lists is empty</h3>
+  <div v-else class="block">
+      <div class="content">
+          <h2>Lists is empty</h2>
+      </div>
   </div>
 </template>
 
 <style scoped>
 .lists {
     margin-top: 40px;
+}
+
+h2 {
+    color: #ffffff;
+    text-align: center;
+}
+.block {
+    display: flex;
+    justify-content: center;
+}
+.content {
+    padding: 50px;
+    max-width: 450px;
+    width: 100%;
+    border: 1px solid #424242;
+    border-radius: 7px;
 }
 </style>
